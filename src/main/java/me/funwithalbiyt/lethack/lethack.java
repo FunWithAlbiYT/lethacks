6dba6edc4f0daa0279ffde82c3c237d01ebf14d8
Torono909 <86355565+Torono909@users.noreply.github.com>
2021-11-20 13:23:32 +0530

    Update lethack.java

--- src/main/java/me/funwithalbiyt/lethack/lethack.java
+++ src/main/java/me/funwithalbiyt/lethack/lethack.java
@@ -1,4 +1,4 @@
-package me.jaxui.nsaclient;
+package me.funwithalbiyt.lethack;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+@Mod(modid = LetHack.MODID, name = LetHack.NAME, version = LetHack.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "nsaclient";
-    public static final String NAME = "Nsa Client";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "lethack";
+    public static final String NAME = "LetHack";
+    public static final String VERSION = "b1.01";
 
     private static Logger logger;
 
@@ -25,7 +25,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("Done B*tch");
     }
 }
